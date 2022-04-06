47f3bdc9cd4baca9eba95cf34959ec4ab634b4ab
CHaBou <chabup@delean.fr>
2022-04-07 00:42:31 +0200

    feat(metric): add JMX for pool metrics

--- server/connectors/deploy-connector-mysql/src/main/scala/com/prisma/deploy/connector/mysql/MySqlInternalDatabaseDefs.scala
+++ server/connectors/deploy-connector-mysql/src/main/scala/com/prisma/deploy/connector/mysql/MySqlInternalDatabaseDefs.scala
@@ -39,6 +39,9 @@ case class MySqlInternalDatabaseDefs(dbConfig: DatabaseConfig, driver: Driver) {
         |  }
         |  numThreads = 1
         |  connectionTimeout = 5000
+        |  registerMbeans = true
+        |  poolName = "graphdb"
+        |  conectionPool = "HikariCP"
         |  $pooled
         |}
       """.stripMargin)
